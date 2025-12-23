7929210170ce911e1cb733b2b56e46f9bdaae4ec
Mark Long <markdlong128@gmail.com>
2025-12-22 23:02:34 -0600

    Moved entry point and DI to Pandowdy project

---
+++ Pandowdy/App.axaml.cs
@@ -0,0 +1,58 @@
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using System;
+using Pandowdy.UI;
+using Pandowdy.UI.ViewModels;
+using Pandowdy.EmuCore;
+using Pandowdy.EmuCore.Interfaces;
+
+namespace Pandowdy;
+
+public partial class App : Application
+{
+    public override void Initialize()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    public override void OnFrameworkInitializationCompleted()
+    {
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            var services = DesktopServiceProvider.Current;
+            var vm = services.GetService(typeof(MainWindowViewModel)) as MainWindowViewModel;
+            var machine = services.GetService(typeof(VA2M)) as VA2M;
+            var frameProvider = services.GetService(typeof(IFrameProvider)) as IFrameProvider;
+            var ticker = services.GetService(typeof(IRefreshTicker)) as IRefreshTicker;
+            var mainWindow = new MainWindow();
+            mainWindow.InjectDependencies(vm!, machine!, frameProvider!, ticker!);
+            desktop.MainWindow = mainWindow;
+        }
+
+        base.OnFrameworkInitializationCompleted();
+    }
+}
+
+public static class DesktopServiceProvider
+{
+    private static IServiceProvider? _provider;
+
+    public static void SetProvider(IServiceProvider provider)
+    {
+        _provider = provider;
+    }
+
+    public static IServiceProvider Current
+    {
+        get
+        {
+            if (_provider == null)
+            {
+                throw new InvalidOperationException("ServiceProvider has not been initialized.");
+            }
+
+            return _provider;
+        }
+    }
+}
--- Pandowdy/UiBootstrap.cs
+++ Pandowdy/UiBootstrap.cs
@@ -1,5 +1,4 @@
 using Microsoft.Extensions.Hosting;
-using Pandowdy.UI;
 
 namespace Pandowdy
 {
